8a5d44249ddcebea17d33b663b3f704e13381432
Michael Lazarski <michael@lazarski.me>
2013-09-17 13:51:33 +0200

    uebung1

--- 00helloworld/main.cpp
+++ 00helloworld/main.cpp
@@ -3,8 +3,8 @@ using namespace std;
 main()
 {
  cout << "Hello World" << endl;
-
  //fuer windows only
+ // wartet auf eingabe sonst blinkt das fenster nur auf
  //int dummy;
  //cin >> dummy;
  return 0;
---
+++ 01uebung01/main.cpp
@@ -0,0 +1,15 @@
+// Aufgabe: Stellen sie sich vor!
+
+#include <iostream>
+#include <string>
+using namespace std;
+main()
+{
+	string name;
+	cout << "Hello, bitte stellen sie sich vor." << endl;
+	cout << "name: ";
+	cin >> name;
+	cout << "hallo," << name << endl;;
+
+	return 0;
+}
